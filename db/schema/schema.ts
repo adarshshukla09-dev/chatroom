b7cab83ded5c3bc8b8cd7ba2580db7e5d95e54a1
Adarsh Shukla <adarshshukla09887@gmail.com>
2026-04-12 17:49:53 +0530

    message schema

--- db/schema/schema.ts
+++ db/schema/schema.ts
@@ -8,9 +8,17 @@ import {
   uuid,
   PgTable,
 } from "drizzle-orm/pg-core";
+import { user } from "./auth-schema";
 
-export const room = pgTable("room",{
-  id: uuid("id").primaryKey(),
-  name:text("name").notNull(),
-message:text("message")
+
+
+export const messages = pgTable("messages",{
+  id: uuid("id").defaultRandom().primaryKey(),
+  senderId: text("sender_id").notNull().references(() => user.id),
+  receiverId: text("receiver_id").notNull().references(() => user.id),
+  content: text("content").notNull(),
+  seen:boolean().default(false),
+
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").defaultNow(),
 })
