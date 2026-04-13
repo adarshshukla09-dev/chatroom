5856d96d9233f371ddc13da06fc594e8aea9c5c2
Adarsh Shukla <adarshshukla09887@gmail.com>
2026-04-13 19:38:43 +0530

    add about,lastseen in users

--- db/schema/auth-schema.ts
+++ db/schema/auth-schema.ts
@@ -7,6 +7,8 @@ export const user = pgTable("user", {
   email: text("email").notNull().unique(),
   emailVerified: boolean("email_verified").default(false).notNull(),
   image: text("image"),
+  lastseen: timestamp("last_seen").defaultNow().notNull(),
+  about: text("about").notNull().default("its time for coding"),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at")
     .defaultNow()
